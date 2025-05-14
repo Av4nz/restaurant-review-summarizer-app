559ef4b1709f858235710af2a1eec81dfe3bf824
CinxZaned <affanarfani4@gmail.com>
2025-05-14 11:23:49 +0700

    Second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0 left-0 w-full bg-slate-700 text-slate-50 z-50'>
-      <div className='container mx-auto flex items-center justify-between p-4'>
-        <div className='text-xl font-bold'>My Website</div>
-        <ul className='flex space-x-4'>
-          <li><a href="#" className='text-slate-50'>Home</a></li>
-          <li><a href="#" className='text-slate-50'>Guide</a></li>
-          <li><a href="#" className='text-slate-50'>Contact</a></li>
+    <nav className="fixed top-0 left-0 w-full bg-slate-700 text-slate-50 z-50">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <div className="text-xl font-bold cursor-pointer">
+          <Link to="/">Review Summarizer</Link>
+        </div>
+        <ul className="flex space-x-4 text-slate-50">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="#">
+              Guide
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Contact
+            </a>
+          </li>
         </ul>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
